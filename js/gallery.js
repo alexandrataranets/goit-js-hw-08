2525146159b2c82fd66c918378fc621a6350388a
alexandrataranets <142220751+alexandrataranets@users.noreply.github.com>
2024-01-19 14:41:09 +0100

    add new script

--- js/gallery.js
+++ js/gallery.js
@@ -94,25 +94,6 @@ const images = [
     e.preventDefault();
   });
 
-// const galleryContainer = document.querySelector('.gallery');
-
-// const createGalleryItem = ({ preview, original, description }) => {
-//   return `
-//     <li class="gallery-item">
-//       <a class="gallery-link" href="${original}">
-//         <img
-//           class="gallery-image"
-//           src="${preview}"
-//           data-source="${original}"
-//           alt="${description}"
-//         />
-//       </a>
-//     </li>
-//   `;
-// };
-
-// const galleryMarkup = images.map(createGalleryItem).join('');
-// galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 galleryContainer.addEventListener('click', onGalleryClick);
 
